5c2ffc3d9c4e87a0e9d5cb51d0c9faa037f1ce10
Magnum2193 <rodriigo.magnum@gmail.com>
2022-09-09 14:56:51 -0300

    Controle de estacionamento Spring

---
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,26 @@
+package com.api.parkingcontrol.services;
+
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class ParkingSpotService {
+
+    //  Injeção de dependencia
+    @Autowired
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository){
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+}
